7dc9c65f8ee3ddac4128fd41fea9a987cc7c265f
TheGreenSprout <131502278+TheGreenSprout@users.noreply.github.com>
2025-11-02 14:41:24 +0100

    Save system fix

--- Scripts/DTO_Classes.cs
+++ Scripts/DTO_Classes.cs
@@ -4,44 +4,61 @@ using System.Collections.Generic;
 namespace SHUU.UserSide
 {
 
-// Take this script out of the package and use it as your own.
-// DTO: Data Transfer Object --> Make your own and add them to MasterDTO (it's the DTO that will get serialized and saved).
+    // DTO: Data Transfer Object --> Make your own and add them to MasterDTO (it's the DTO that will get serialized and saved).
 
-[Serializable]
-#region XML doc
-/// <summary>
-/// DTO that holds an instance of all the other DTOs (this one gets serialized and saved).
-/// </summary>
-#endregion
-public class MasterDTO
-{
-    // This is how your MasterDTO should look (with all your data DTOs).
-    public DTO_ExampleInfo exampleData;
-}
+    [Serializable]
+    #region XML doc
+    /// <summary>
+    /// DTO that holds an instance of all the other DTOs (this one gets serialized and saved).
+    /// </summary>
+    #endregion
+    public class MasterDTO
+    {
+        public Dictionary<DTO_Info, string> dataDictionary;
 
 
-
-// This is a parent DTO class used to identify all DTOs, keep it.
-[Serializable]
-#region XML doc
-/// <summary>
-/// Parent to all DTOs.
-/// </summary>
-#endregion
-public class DTO_InfoClass { }
+        public MasterDTO()
+        {
+            // This is how your MasterDTO should look (with all your data DTOs).
+            DTO_Info exampleData = new DTO_ExampleInfo();
 
 
+            dataDictionary = new Dictionary<DTO_Info, string>
+            {
+                // This is how your MasterDTO should look (with all your data DTOs).
+                { exampleData, exampleData.GetIdentifier() },
+            };
+        }
+    }
 
-// This is how you should make all your DTO classes:
-[Serializable]
-#region XML doc
-/// <summary>
-/// Example of how to make your own DTOs.
-/// </summary>
-#endregion
-public class DTO_ExampleInfo : DTO_InfoClass
-{
-    public Dictionary<int, bool> colorOfItems = new Dictionary<int, bool>();
-}
+
+
+    // This is a parent DTO class used to identify all DTOs, keep it.
+    #region XML doc
+    /// <summary>
+    /// Interface to all DTOs.
+    /// </summary>
+    #endregion
+    public interface DTO_Info
+    {
+        public string GetIdentifier();
+    }
+
+
+
+    // This is how you should make all your DTO classes:
+    [Serializable]
+    #region XML doc
+    /// <summary>
+    /// Example of how to make your own DTOs.
+    /// </summary>
+    #endregion
+    public class DTO_ExampleInfo : DTO_Info
+    {
+        public string GetIdentifier() { return "Example"; }
+        
+        
+        public Dictionary<int, bool> colorOfItems = new Dictionary<int, bool>();
+    }
 
 }
